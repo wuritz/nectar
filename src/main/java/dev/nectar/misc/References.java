903cf9a6c520403c84ec01957cb5b55244436f75
wuritz <ritzke06@gmail.com>
2026-01-16 15:08:19 +0100

    update version

--- src/main/java/dev/nectar/misc/References.java
+++ src/main/java/dev/nectar/misc/References.java
@@ -1,8 +1,5 @@
 package dev.nectar.misc;
 
 public class References {
-    public static String CLIENT_NAME = "Sedna Client";
-    public static String CLIENT_VERSION = "Alpha 1.6";
-    public static String CLIENT_AUTHOR = "TheBlueStoplight";
-    public static String CLIENT_CONTRIB = "wuritz";
+    public static String CLIENT_NAME = "Nectar";
 }
--- src/main/java/dev/nectar/systems/hud/HUD.java
+++ src/main/java/dev/nectar/systems/hud/HUD.java
@@ -1,6 +1,8 @@
 package dev.nectar.systems.hud;
 
+import dev.nectar.Nectar;
 import dev.nectar.events.core.render.Render2DEvent;
+import dev.nectar.events.core.render.Render3DEvent;
 import dev.nectar.misc.References;
 import dev.nectar.systems.System;
 import dev.nectar.ui.UIUtils;
@@ -19,10 +21,13 @@ public class HUD extends System<HUD> implements ISerializable<HUD> {
 
     @EventHandler
     public void onRender2D(Render2DEvent event) {
-        if (!mc.inGameHud.getDebugHud().shouldShowDebugHud()) {
-            event.drawContext.fill(0, 0, mc.textRenderer.getWidth(References.CLIENT_NAME + " " + References.CLIENT_VERSION) + margin + (margin / 4), mc.textRenderer.fontHeight + margin, UIUtils.BACKGROUND_BASE.getRGB());
-            event.drawContext.fill(mc.textRenderer.getWidth(References.CLIENT_NAME + " " + References.CLIENT_VERSION) + margin, 0, mc.textRenderer.getWidth(References.CLIENT_NAME + " " + References.CLIENT_VERSION) + margin + (margin / 4), mc.textRenderer.fontHeight + margin, UIUtils.getSelectedPrimaryColor().getRGB());
-            event.drawContext.drawTextWithShadow(mc.textRenderer, References.CLIENT_NAME + " " + References.CLIENT_VERSION, margin / 2, margin / 2, UIUtils.getSelectedPrimaryColor().getRGB());
-        }
+        if (mc.inGameHud.getDebugHud().shouldShowDebugHud()) return;
+
+        String version = Nectar.VERSION.toString();
+        String client = "nectar";
+
+        event.drawContext.fill(0, 0, mc.textRenderer.getWidth(client + " " + version) + margin + (margin / 4), mc.textRenderer.fontHeight + margin, UIUtils.BACKGROUND_BASE.getRGB());
+        event.drawContext.fill(mc.textRenderer.getWidth(client + " " + version) + margin, 0, mc.textRenderer.getWidth(client + " " + version) + margin + (margin / 4), mc.textRenderer.fontHeight + margin, UIUtils.getSelectedPrimaryColor().getRGB());
+        event.drawContext.drawTextWithShadow(mc.textRenderer, client + " " + version, margin / 2, margin / 2, UIUtils.getSelectedPrimaryColor().getRGB());
     }
 }
